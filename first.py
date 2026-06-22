9cb1f0788ea873fa87970b1f9f955e5f26e4bea1
shivanimalik123 <malikshivani04789@gmail.com>
2026-06-22 21:39:26 +0530

    Add first Python file

---
+++ first.py
@@ -0,0 +1,60 @@
+# print("hello")
+# a=3
+# b=4
+# print(a+b)
+# a=["s","s","v"]
+# a.append("k")
+# print(a)
+# a.inse/rt(0,"b")
+# print(a)
+# prin/t("h/ello suman")
+# a=4/
+# b=6
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+# print(a**b)
+# for i in range (1,4):
+#   for j in range (1,5):
+    # print("hunny")
+# for i in range (1,5):
+    # for j in range (1,5):
+        # for k in range(1,6):
+        #  print(i,j)
+# for i in range(1,4):
+#    for j in range(1,4):
+    #    print("*")  
+# for i in range(1,3):
+    #  for j in range(1,3):      
+        #  print("i=", i, "j=", j)
+# for i in range(1,6):
+    # print("rittu")  
+# for i in range(1,2):
+    # for j in range(1,2):
+        # print(i)
+        # print(j)  
+        # print(i,j) 
+# for i in range(1,5):
+    # for j in range(1,3):
+        # for k in range(1,4):
+        #   print(j)
+a=int("3")
+b=4
+# print(a+b) 
+name=None
+# print(type(name))  
+a=4
+a%=4
+# print(a)
+a=True
+b=False
+print(a or b)
+print(not(True))
+a=5
+b=3
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(a << b )
+print(5  > 2 and 10 > 3)
